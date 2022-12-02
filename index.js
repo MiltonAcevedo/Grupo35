8d6583843337453194d58a16020e191f67529849
MiltonAcevedo <miltongabrielacevedoperez@gmail.com>
2022-12-01 21:46:37 -0500

    nodemon express

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/',  (req, res) => {
   res.send('bienvenidos 🐱‍🚀a la clase de desarrollo web')
 })
-
+const puerto= 8000;
 app.listen(8000, ()=> {
  console.log ("el servidor esta corriendo en http://localhost:8000")
 
